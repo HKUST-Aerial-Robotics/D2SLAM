f934087b4dc4b57b0f52ea1e81ce24a488f7d65d
Xu Hao <xuhao3e8@gmail.com>
2022-05-31 05:12:06 +0800

    Add param: remote_min_match_num.

--- d2frontend/src/d2frontend_params.cpp
+++ d2frontend/src/d2frontend_params.cpp
@@ -68,6 +68,7 @@ namespace D2FrontEnd {
         ftconfig->long_track_frames = fsSettings["landmark_estimate_tracks"];
         ftconfig->check_homography = (int) fsSettings["check_homography"];
         ftconfig->enable_lk_optical_flow = (int) fsSettings["enable_lk_optical_flow"];
+        ftconfig->remote_min_match_num = fsSettings["remote_min_match_num"];
         vlad_threshold = fsSettings["vlad_threshold"];
         nh.param<int>("long_track_thres", ftconfig->long_track_thres, 20);
         nh.param<int>("last_track_thres", ftconfig->last_track_thres, 20);
