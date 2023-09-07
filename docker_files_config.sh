e2692f763bdf4f2649ea8864a32ad12d4d937e04
MortyL <mortyl0834@gmail.com>
2023-09-07 16:31:16 +0800

    fix docker start file combined with git version

---
+++ docker_files_config.sh
@@ -0,0 +1,8 @@
+SWARM_WS=/root/swarm_ws
+DOCKERIMAGE="d2slam:pc"
+LOCAL_WS=/path/to/your/local_dir
+DATA_SET=/path/to/your/dataset
+
+CONFIGS=${LOCAL_WS}/configs-drone-swarm
+HITNET=${LOCAL_WS}/perception-ONNX-HITNET-Stereo-Depth-estimation
+CRESTEREO=${LOCAL_WS}/perception-ONNX-CREStereo-Depth-Estimation
--- start_docker.sh
+++ start_docker.sh
@@ -2,11 +2,8 @@
 # Usage: ./start_docker.sh  0 to start docker with current file-dir, anything you modify in docker will be saved in current file-dir
 # Usage: ./start_docker.sh  1 to start docker only for image transportation.
 # Please do not move this file to other dir, it will cause the docker container can not find the current dir.
-SWARM_WS=/root/swarm_ws
-DOCKERIMAGE="d2slam:pc"
-DATA_SET=/media/khalil/ssd_data/data_set
-HITNET=/home/khalil/workspace/d2slam_ws/src/ONNX-HITNET-Stereo-Depth-estimation
-CRESTEREO=/home/khalil/workspace/d2slam_ws/src/ONNX-CREStereo-Depth-Estimation
+
+source ./docker_files_config_local.sh
 
 if [ $# -eq 0 ]; then
   echo "[INFO] No start option, will start docker container only for application"
